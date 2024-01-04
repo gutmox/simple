87636ef0c5593ec106d960f4c36647296e1b5b48
Pablo Gutierrez <gutmox@gmail.com>
2024-01-04 18:09:15 +0100

    Adding libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,9 @@ repositories {
 dependencies {
     implementation("io.vertx:vertx-pg-client:4.5.1")
     implementation("com.ongres.scram:client:2.1")
+    implementation("com.github.f4b6a3:ulid-creator:5.2.2")
     testImplementation("org.assertj:assertj-core:3.25.0")
+    testImplementation("org.awaitility:awaitility:4.2.0")
 }
 
 testing {
